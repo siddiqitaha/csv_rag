01c758fdda80195b8e4e8165ccc2856ef8c30f18
Taha Nasir Siddiqi <92089684+siddiqitaha@users.noreply.github.com>
2024-06-20 01:30:35 +0300

    Update installation.py

--- installation.py
+++ installation.py
@@ -1,3 +1,8 @@
 import os
+
+# Install required packages
 os.system('pip install llama-index-core')
 os.system('pip install llama-index-embeddings-huggingface')
+os.system('pip install llama-index')
+os.system('pip install chromadb')
+os.system('pip install llama-index-vector-stores-chroma')
